f307edb5d15a03782b7498b0e140b198201e9862
Blini73 <blinmehmeti10@gmail.com>
2024-11-04 17:47:10 +0100

    shgasihg

--- lesson_5/index.php
+++ lesson_5/index.php
@@ -1,9 +1,25 @@
 <?php
 $dogs = array (
     array("Chihuahua","Mexico",20),
-    array("Husky","Siberia",15),
-    array("Bulldog","England",10),
+    array("Husky","Siberia", 15),
+    array("Bulldog","England", 10)
 );
 
-echo $dogs[0][0].  "Origin:"   .$dogs[0][1]. ":life span:" . $dogs[0][2]. ""
+echo $dogs[0][0].  " Origin: "   .$dogs[0][1]. ",life span: " . $dogs[0][2]. "<br>";
+echo $dogs[1][0].  " Origin: "   .$dogs[1][1]. ",life span: " . $dogs[1][2]. "<br>";
+echo $dogs[2][0].  " Origin: "   .$dogs[2][1]. ", life span: " . $dogs[2][2]. "<br>";
+
+for($row = 0; $row<3; $row++){
+    echo "<p><br>Row number $row</b></p>";
+    echo "<ul>";
+    for ($col = 0; $col<3; $col++){
+        echo "<li>".$dogs[$row][$col]."</li>";
+    }
+    echo "</ul>";
+}
 ?>
+
+
+
+
+
