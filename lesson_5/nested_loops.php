e4c742110fcfe511075622e9de397abc70d6718b
Blini73 <blinmehmeti10@gmail.com>
2024-11-06 17:04:54 +0100

    Update nested_loops.php

--- lesson_5/nested_loops.php
+++ lesson_5/nested_loops.php
@@ -1,13 +1,40 @@
 <?php
-$arrays = array(
-    array(1,2,3),
-    array(1,2,3),
-    array(1,2,3),
-);
+//$arrays = array(
+  //  array(1,2,3),
+   // array(1,2,3),
+   // array(1,2,3),
+//);
 
-for($i = 0; $i < 4; $i++){
-    for($j = 1; $j < 4; $j++){
-        echo "Array: $i Element: $j <br>";
-    }
-}
+//for($i = 0; $i < 4; $i++){
+ //   for($j = 1; $j < 4; $j++){
+  //      echo "Array: $i Element: $j <br>";
+   // }
+//}
+ // for($i=1;$i=5; $i++){
+  //  for($j=1; $j<$i; $j ++){
+   //     echo "*";
+    //}
+   // echo "<br/>";
+  //}
+
+
+
+
+
+    $grade = array(
+        "Math" => "3",
+        "Art" => "5",
+        "History" => "4",
+        "Music" => "4",
+    );
+
+    //echo "math grade is" .$grade['Math'];
+  
+  foreach($grade as $subject =>  $grade ){
+    echo "Subject: ".subject . " /Grade:  ".$grade;
+    echo "<br>";
+  }
+    
+
+
 ?>
