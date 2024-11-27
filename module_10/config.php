d21195bf90811065c75f9c6e7fbeb10250eeaf16
Blini73 <blinmehmeti10@gmail.com>
2024-11-27 17:47:38 +0100

    sqwkfhasoh

---
+++ module_10/config.php
@@ -0,0 +1,16 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "another_db";
+
+
+try{
+    $connect = new PDO("mysql:host=$server;dbname=$dbname",$username, $password);
+
+    // echo "Connected!!!";
+}
+catch(PDOException $e){
+    echo "somthing went wrong!!";
+}
+?>
