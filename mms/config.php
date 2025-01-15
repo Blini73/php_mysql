08b50eacb3daf5b1f692fd59f8b1b143a7354cba
Blini73 <blinmehmeti10@gmail.com>
2025-01-15 17:47:10 +0100

    asas

---
+++ mms/config.php
@@ -0,0 +1,13 @@
+<?php 
+$server = "localhost";
+$dbname = "mms";
+$user = "root";
+$pass = "";
+
+try{
+    $conn = new PDO("mysql:host=$server;dbname=$dbname", $user, $pass);
+}catch(PDOException $e){
+    echo "error: ".$e.getMessage();
+}
+
+?>
